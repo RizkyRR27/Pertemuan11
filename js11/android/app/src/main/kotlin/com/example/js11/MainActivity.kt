c53abfb5681141f9e5c3b190723f016b21f8e6ee
RizkyRR27 <rizkyroza2005@gmail.com>
2025-11-16 17:48:27 +0700

    first commit

---
+++ js11/android/app/src/main/kotlin/com/example/js11/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.js11
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ js11/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:js11/view.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class AppSettings {
+  bool isDarkMode = false;
+  String userName = 'Teman';
+  int fontSize = 20;
+  
+
+  Color get backgroundColor {
+    return isDarkMode ? Colors.grey[900]! : Colors.white;
+  }
+
+  Color get textColor {
+    return isDarkMode ? Colors.white : Colors.black;
+  }
+
+  String get greeting {
+    return "Halo, $userName!";
+  }
+}
